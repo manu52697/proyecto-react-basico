f98ae1738d45145a7ac9ab04fdf907b1c2eb4de5
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-07-11 07:51:53 +0200

    Add fake task data

---
+++ src/util/fake/taskPopulator.js
@@ -0,0 +1,26 @@
+import { LEVELS } from "../../models/levels.enum";
+import { Task } from "../../models/task.class";
+
+export const TASK_LIST_DEMO = [
+    new Task(
+        'Example task 1', 
+        'Aliqua sit velit magna ut. Reprehenderit in adipisicing elit fugiat ut reprehenderit fugiat mollit sit. Est commodo veniam sint sint adipisicing sint enim labore sint pariatur.',
+        false,
+        LEVELS.PRIORITY
+        ),
+        new Task(
+            'Example task 2',
+            'Proident labore pariatur ipsum officia exercitation elit duis nostrud.',
+            true,
+            LEVELS.BLOCKING
+        ),
+        new Task(
+            'Example task 3',
+            'Amet laborum ad quis reprehenderit commodo. Adipisicing deserunt labore est minim amet aliquip ipsum adipisicing cillum nulla irure aliqua nostrud. Eu aute veniam nostrud voluptate amet cillum ex incididunt mollit do culpa.',
+            false,
+            LEVELS.NORMAL
+
+        )
+];
+
+TASK_LIST_DEMO.forEach((t, i) => t.id=i);
