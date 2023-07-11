8020391fc3988893a3ecde24046abda128077a07
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-07-11 09:15:50 +0200

    Migrate 404 page to tsx

---
+++ src/pages/404/NotFoundPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+const NotFoundPage: React.FunctionComponent = () => {
+
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <div>
+            <h1>Oooops! We couldn't find that!</h1>
+        </div>
+    );
+}
+
+export default NotFoundPage;
