ecf9f0a2bb8cdac6e4fecbbddc698612f6e245fa
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-07-12 08:26:23 +0200

    Add logout to profile page and navigation there from home

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,17 @@
 import type React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage: React.FunctionComponent = () => {
+  const navigate = useNavigate()
+
+  const goToProfile = (): void => {
+    navigate('/profile')
+  }
   return (
         <div>
             <h1>Home Page</h1>
             <h2>Dashboard</h2>
+            <button onClick={goToProfile}>My profile</button>
         </div>
   )
 }
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import type React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { sessionRemove } from '../../util/session-storage'
 
 // import { User } from '../../models/user.class';
 
@@ -10,10 +11,16 @@ import { useNavigate } from 'react-router-dom'
 const ProfilePage: React.FunctionComponent = () => {
   const navigate = useNavigate()
 
+  const onLogout = (): void => {
+    sessionRemove('credentials')
+    navigate('/home')
+  }
+
   return (
         <div>
             <h1>Your profile</h1>
             <button onClick={() => { navigate('/tasks') }}>Go to tasks</button>
+            <button onClick={onLogout}>Log Out</button>
         </div>
   )
 }
